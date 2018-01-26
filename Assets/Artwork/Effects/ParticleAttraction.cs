d07e0a3ef7ea877b58e926157f15fd354c1426ed
AIE\sam.nicholas <samrbnicholas@gmail.com>
2018-01-27 09:04:37 +1030

    Environment assets Push, some decorative items

---
+++ Assets/Artwork/Effects/ParticleAttraction.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class ParticleAttraction : MonoBehaviour
+{
+    public Transform Target;
+    public float speed;
+
+    private ParticleSystem system;
+
+    private static ParticleSystem.Particle[] particles = new ParticleSystem.Particle[1000];
+
+    void Update()
+    {
+        if (system == null) system = GetComponent<ParticleSystem>();
+
+        var count = system.GetParticles(particles);
+
+        for (int i = 0; i < count; i++)
+        {
+            var particle = particles[i];
+
+            float distance = Vector3.Distance(Target.position, particle.position);
+
+            if (distance > 0.1f)
+            {
+                particle.position = Vector3.Lerp(particle.position, Target.position, (Time.deltaTime / 2.0f) * speed);
+
+                particles[i] = particle;
+            }
+        }
+
+        system.SetParticles(particles, count);
+    }
+}
