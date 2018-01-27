e103d79cc19801f779af584519d4bb523e4360a4
AIE\adam.bailey <adambailey@aie.edu.au>
2018-01-28 04:10:04 +1030

    Intro video added, tutorial level has extra art assets added

---
+++ Assets/Scripts/IntroVideoScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroVideoScript : MonoBehaviour {
+
+    public UnityEngine.Video.VideoClip introVid;
+    UnityEngine.Video.VideoPlayer videoPlayer;
+    public string nextLevelToLoad = "MainMenu";
+
+	// Use this for initialization
+	void Start () {
+        videoPlayer = gameObject.AddComponent<UnityEngine.Video.VideoPlayer>();
+        videoPlayer.renderMode = UnityEngine.Video.VideoRenderMode.CameraNearPlane;
+        videoPlayer.clip = introVid;
+	}
+
+    void Update()
+    {
+        float currentFrame = videoPlayer.frame;
+        float frameCount = videoPlayer.frameCount;
+        if (currentFrame == frameCount)
+        {
+            SceneManager.LoadScene(nextLevelToLoad);
+        }
+    }
+
+}
