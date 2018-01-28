f6e97089781b1eec91d80e2ae667532970e59ce9
AIE\johnm <johnm@aie.edu.au>
2018-01-28 14:20:42 +1030

    Added reset scene trigger

---
+++ Assets/Scripts/ResetScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetScene : MonoBehaviour
+{
+    void OnTriggerEnter(Collider c)
+    {
+        if (c.tag == "Player")
+        {
+            StartAndEndEffects startAndEndEffects = FindObjectOfType<StartAndEndEffects>();
+            startAndEndEffects.nextLevel = SceneManager.GetActiveScene().name;
+            startAndEndEffects.EndLevel();
+        }
+    }
+}
