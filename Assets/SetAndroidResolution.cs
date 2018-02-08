7e31c2c4205a4489ce65a8cb2e17006e9caf5adc
AIE\adam.bailey <adambailey@aie.edu.au>
2018-02-08 15:28:54 +1030

    Testing out an Android build for funsies

---
+++ Assets/SetAndroidResolution.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetAndroidResolution : MonoBehaviour {
+
+    public int hRenderRes = 1480;
+    public int vRenderRes = 720;
+
+    // Use this for initialization
+#if UNITY_ANDROID
+    void Start () {
+        Screen.SetResolution(hRenderRes, vRenderRes , true);
+    }
+#endif
+
+}
